ec9182c6732a6675e6362139ef601fe85d64b46f
Travis Webb <me@traviswebb.com>
2014-07-10 13:49:49 -0400

    removed npmlog dependency

--- index.js
+++ index.js
@@ -6,23 +6,31 @@ global.cwd = process.cwd();
 
 var nex = module.exports = {
 
+  /**
+   * Creates an npm module id with which npm install can be used
+   */
   id: function (version, name) {
     return name + (version ? ('@' + version) : '');
   },
 
+  /**
+   * Converts the name of a nex module to a package.json field name
+   */
   toField: function (field) {
     return 'nex-'+ field.replace(/[_.-](\w|$)/g, function (_, x) {
       return x.toUpperCase();
     });
   },
 
+  /**
+   * Converts a field name to the name of a nex module
+   */
   toNpm: function (field) {
     return 'nex-'+ field.replace(/([a-z])([A-Z0-9])/g, '$1-$2').toLowerCase();
   },
 
   Handler: function (field) {
-    this.log = require('npmlog');
-    this.log.heading = field;
+    this.field = field;
 
     this.do = function (field, pkg) {
       throw new Error('Handler.do must be overridden');
